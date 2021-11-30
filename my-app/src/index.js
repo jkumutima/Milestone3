33640a91714a461e817089f9882c00aa786692b3
jkumutima <89998428+jkumutima@users.noreply.github.com>
2021-11-29 20:05:47 -0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -34,7 +39,7 @@ ReactDOM.render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
+//  measure performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log)) 
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
